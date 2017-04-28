9aa352d08c5973e8ac72ffa445b97f3e1a905e32
Brian Tom <btom.0831@gmail.com>
2017-04-27 18:21:43 -0700

    Initial test files commit with simple animation

---
+++ dwb/test/scroll.py
@@ -0,0 +1,63 @@
+import pygame
+
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((500, 500), pygame.DOUBLEBUF | pygame.HWSURFACE)
+    width, height = screen.get_size()
+
+    text = pygame.font.Font(None, 100).render('Test', True, (255, 255, 255))
+    twidth, theight = text.get_size()
+
+    image = pygame.image.load('test.png')
+    iwidth, iheight = image.get_size()
+    scale = width/iwidth  # scale by width
+    image = pygame.transform.scale(image, (int(scale*iwidth), int(scale*iheight)))
+
+    y_0 = y = -(theight + iheight)
+
+    speed_0_y = speed_y = 0  # initial speed and current speed
+    accel_y = 1.0  # acceleration of falling images
+    stop_y = 100  # stop y position for images
+
+    clock = pygame.time.Clock()
+
+    running = True
+    waited = False
+
+    while running:
+        # Check for events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                break
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+                    break
+        # Draw onto screen
+        screen.fill(0)
+        screen.blit(text, (0, y))
+        screen.blit(image, (0, y))
+        # Update y position
+        if y >= height:
+            y = y_0  # reset y position to starting position
+            waited = False
+        else:
+            speed_y += accel_y
+            y += speed_y
+        # Update screen
+        pygame.display.flip()
+        # Pause once per cycle
+        if not waited and y >= stop_y:
+            print(y)
+            waited = True
+            pygame.time.wait(1000)
+            speed_y = speed_0_y  # reset speed to initial speed
+        # 60 FPS
+        clock.tick(60)
+
+
+if __name__ == '__main__':
+    main()
