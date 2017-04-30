a6dc1d7fb1ae4201dd1f949b4a802d00f1f5a0e6
Brian Tom <btom.0831@gmail.com>
2017-04-30 14:08:09 -0700

    Update scroll with multiple images in directory

--- dwb/test/scroll.py
+++ dwb/test/scroll.py
@@ -8,24 +8,21 @@ Created Apr 29, 2017
 
 
 import pygame
+from pathlib import Path
 
 
 def main():
     pygame.init()
 
+    # screen
     screen = pygame.display.set_mode((500, 500), pygame.DOUBLEBUF | pygame.HWSURFACE)
     width, height = screen.get_size()
 
+    # test text
     text = pygame.font.Font(None, 100).render('Test', True, (255, 255, 255))
     twidth, theight = text.get_size()
 
-    image = pygame.image.load('test.png')
-    iwidth, iheight = image.get_size()
-    scale = width/iwidth  # scale by width
-    image = pygame.transform.scale(image, (int(scale*iwidth), int(scale*iheight)))
-
-    y_0 = y = -(theight + iheight)
-
+    # physics
     speed_0_y = speed_y = 0  # initial speed and current speed
     accel_y = 0.3  # acceleration of falling images
     stop_y = 100  # stop y position for images
@@ -33,8 +30,21 @@ def main():
 
     clock = pygame.time.Clock()
 
+    # track cycling of frames
     running = True
     waited = False
+
+    # image settings
+    last_index = None
+    curr_index = 0
+    image_paths = list(Path('images/').glob('**/*.png'))
+
+    # first image
+    image = pygame.image.load(str(image_paths[curr_index]))
+    iwidth, iheight = image.get_size()
+    scale = width/iwidth  # scale by width
+    image = pygame.transform.scale(image, (int(scale*iwidth), int(scale*iheight)))
+    y_0 = y = -(theight + iheight)
 
     while running:
         # Check for events
@@ -46,6 +56,16 @@ def main():
                 if event.key == pygame.K_ESCAPE:
                     running = False
                     break
+        # image details change when index changes
+        # so updates only once per given frame
+        # TODO: caching?
+        if last_index is not None and last_index != curr_index:
+            last_index = curr_index  # update last index
+            image = pygame.image.load(str(image_paths[curr_index]))
+            iwidth, iheight = image.get_size()
+            scale = width/iwidth  # scale by width
+            image = pygame.transform.scale(image, (int(scale*iwidth), int(scale*iheight)))
+            y_0 = y = -(theight + iheight)
         # Draw onto screen
         screen.fill(0)
         screen.blit(text, (0, y))
@@ -53,7 +73,8 @@ def main():
         # Update y position
         if y >= height:
             y = y_0  # reset y position to starting position
-            waited = False
+            waited = False  # after pausing for specified time
+            last_index, curr_index = curr_index, (curr_index + 1) % len(image_paths)  # cycle through images
         else:
             speed_y += accel_y
             y += speed_y
