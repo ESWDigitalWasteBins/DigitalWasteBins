9747eee0db38292e6e201490071b547a880bd5f1
Brian Tom <btom.0831@gmail.com>
2017-05-01 11:10:32 -0700

    Update scrolling for any number of images - new comments added - screen switched to vertical format - disabled hardware surfaces for Linux compatibility - fixed bug that prevented just a single image from scrolling - fixed image starting position to factor in scaling - added some debug messages

--- dwb/test/scroll.py
+++ dwb/test/scroll.py
@@ -14,36 +14,38 @@ from pathlib import Path
 def main():
     pygame.init()
 
-    # screen
-    screen = pygame.display.set_mode((500, 500), pygame.DOUBLEBUF | pygame.HWSURFACE)
+    # screen to draw on
+    screen = pygame.display.set_mode((506, 900))
     width, height = screen.get_size()
 
     # test text
-    text = pygame.font.Font(None, 100).render('Test', True, (255, 255, 255))
+    text = pygame.font.Font(None, 100).render('Test', True, (0, 0, 0))
     twidth, theight = text.get_size()
 
     # physics
     speed_0_y = speed_y = 0  # initial speed and current speed
-    accel_y = 0.3  # acceleration of falling images
-    stop_y = 100  # stop y position for images
-    wait_time = 5  # seconds
+    accel_y = 0.3            # acceleration of falling images
+    stop_y = 100             # stop y position for images
+    wait_time = 1            # in seconds
 
+    # framerate clock
     clock = pygame.time.Clock()
 
     # track cycling of frames
-    running = True
-    waited = False
+    running = True  # True if frames are cycling
+    waited = False  # True if the frame has waited for wait_time seconds
 
-    # image settings
-    last_index = None
-    curr_index = 0
-    image_paths = list(Path('images/').glob('**/*.png'))
+    # image selection and location settings
+    image_paths = list(Path('images/').glob('**/*.png'))  # get all PNG images from directory
+    last_index = None  # index of the previous image
+    curr_index = 0     # index of the current index
 
-    # first image
-    image = pygame.image.load(str(image_paths[curr_index]))
-    iwidth, iheight = image.get_size()
-    scale = width/iwidth  # scale by width
+    # FIXME: first image, remove duplicate code
+    image = pygame.image.load(str(image_paths[curr_index])).convert()
+    iwidth, iheight = image.get_size()  # image dimensions
+    scale = width/iwidth  # scale factor by width (for now, will change)
     image = pygame.transform.scale(image, (int(scale*iwidth), int(scale*iheight)))
+    iwidth, iheight = image.get_size()  # image dimensions after scaling
     y_0 = y = -(theight + iheight)
 
     while running:
@@ -56,35 +58,41 @@ def main():
                 if event.key == pygame.K_ESCAPE:
                     running = False
                     break
-        # image details change when index changes
-        # so updates only once per given frame
+        # Image details change when index changes,
+        # so image updates only once per given frame
         # TODO: caching?
         if last_index is not None and last_index != curr_index:
+            print('UPDATE @', y, 'last:', last_index, 'curr:', curr_index)
             last_index = curr_index  # update last index
-            image = pygame.image.load(str(image_paths[curr_index]))
-            iwidth, iheight = image.get_size()
+            image = pygame.image.load(str(image_paths[curr_index])).convert()
+            iwidth, iheight = image.get_size()  # image dimensions
             scale = width/iwidth  # scale by width
             image = pygame.transform.scale(image, (int(scale*iwidth), int(scale*iheight)))
+            iwidth, iheight = image.get_size()  # image dimensions after scaling
             y_0 = y = -(theight + iheight)
-        # Draw onto screen
-        screen.fill(0)
+        # Draw onto screen at y position
+        screen.fill((255, 255, 255))
         screen.blit(text, (0, y))
-        screen.blit(image, (0, y))
+        screen.blit(image, (0, y+theight))
         # Update y position
         if y >= height:
-            y = y_0  # reset y position to starting position
+            print('NEXT @', y)
+            y = y_0  # move image back to top
+            screen.fill((255, 255, 255))  # white background
             waited = False  # after pausing for specified time
-            last_index, curr_index = curr_index, (curr_index + 1) % len(image_paths)  # cycle through images
+            # cycle through image indexes
+            last_index, curr_index = curr_index, (curr_index + 1) % len(image_paths)
         else:
-            speed_y += accel_y
-            y += speed_y
+            speed_y += accel_y  # accelerate image
+            y += speed_y  # change image position by speed amount
         # Update screen
         pygame.display.flip()
         # Pause once per cycle
         if not waited and y >= stop_y:
-            print(y)
+            print('STOP @', y)
             waited = True
-            pygame.time.wait(wait_time * 1000)
+            # FIXME: make y position static instead of pausing execution
+            pygame.time.wait(wait_time * 1000)  # pause execution
             speed_y = speed_0_y  # reset speed to initial speed
         # 60 FPS
         clock.tick(60)
