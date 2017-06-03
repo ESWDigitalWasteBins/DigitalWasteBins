eec34ab9afda6bd90137754e363e90d22d355f82
Brian Tom <btom.0831@gmail.com>
2017-06-02 18:42:44 -0700

    Add another frame

--- dwb/display.py
+++ dwb/display.py
@@ -204,7 +204,7 @@ if __name__ == '__main__':
                     screen.get_width(), header_height,
                     'RECYCLE', BLUE)
 
-    frame_count = 1
+    frame_count = 2
     content_per_frame = 2
 
     all_content_height = int(CONTENT_RATIO*screen.get_height())
