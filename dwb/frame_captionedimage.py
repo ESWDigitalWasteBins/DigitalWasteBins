cccfe8c5c9a7e11d895bb871914e50c60e0542c0
Brian Tom <btom.0831@gmail.com>
2017-10-03 21:05:50 -0700

    Separate CaptionedImage

--- dwb/frame_captionedimage.py
+++ dwb/frame_captionedimage.py
@@ -2,6 +2,7 @@ import pygame
 import images
 from pathlib import Path
 from frame import Frame
+from frame_text import TextFrame
 
 
 class CaptionedImage(Frame):
@@ -9,7 +10,10 @@ class CaptionedImage(Frame):
     def __init__(self, screen: pygame.display, parent: Frame,
                  image_path: Path, text: str,
                  padx: int=0, pady: int=0,
-                 bg_color: (int, int, int)=(0, 0, 0)) -> None:
+                 bg_color: (int, int, int)=(0, 0, 0),
+                 text: str='CaptionedImage', text_color: (int, int, int)=(0, 0, 0),
+                 text_padx: int=0, text_pady: int=0,
+                 bg_color: (int, int, int)=(255, 255, 255)) -> None:
         """
         [SUMMARY]
 
@@ -29,6 +33,8 @@ class CaptionedImage(Frame):
         self._image_rect = self._image.get_rect(midtop=(self.get_center()[0], self.get_y()))
         self._text = pygame.font.Font(None, 100).render(text, True, (255, 255, 255))
         self._text_rect = self._text.get_rect(midbottom=(self.get_center()[0], self.get_y()+self.get_height()))
+        # new
+        self._text_frame = TextFrame(screen, self, font_file, text, text_color, text_padx, text_pady)
 
     def draw(self):
         self._update_position()
