bd250a6fdd91e97acc7bafa6b32b69085f982c6b
khoitd1997 <khoidinhtrinh@gmail.com>
2017-11-22 18:16:33 -0800

    Revert "Update display class"
    This reverts commit 7d2518dbf872897cde6be3d0a6c2c576a5e3ac87.

--- dwb/combined_code.py
+++ dwb/combined_code.py
@@ -91,15 +91,12 @@ def _debug_print(*args, **kwargs) -> None:
 
 
 class Display(Frame):
-    def __init__(self, header: Header, bodies: [Body], scale_frame):
+    def __init__(self, header: Header, bodies: [Body]):
         Frame.__init__(self, screen, None, 0, 0,
                        screen.get_width(), screen.get_height())
-        self.screen = screen
         self._header = header
         self._bodies = bodies
-        self.scale_frame = scale_frame
         # Content() to display
-        self.is_using_scale = False
         self._last_index = None
         self._curr_index = 0
         # settings for frame position and rotation
@@ -118,10 +115,7 @@ class Display(Frame):
 
     def draw(self):
         """Blit animations to screen."""
-        if self.is_using_scale:
-            curr_body = self.scale_frame
-        else:
-            curr_body = self._bodies[self._curr_index]
+        curr_body = self._bodies[self._curr_index]
 
         if self._stop_y is None or (self._last_index is not None and self._last_index != self._curr_index):
             _debug_print('UPDATE @', self._y, 'last:',
@@ -250,14 +244,14 @@ if __name__ == '__main__':
                 i + j + 1), 0, header.get_height() + j * content_height, screen.get_width(), content_height))
         body_list.append(sub_list)
 
+    # Display
+    display = Display(header, body_list)
+
     # Scale Reading Test
     scale = Scale()
     prev_reading = scale.check()
-    scale_frame = TextFrame(
-        screen, None, text=str(prev_reading), text_color=(255, 255, 0))
-
-    # Display
-    display = Display(header, body_list, scale_frame)
+    scale_reading = TextFrame(
+        screen, display, text=str(prev_reading), text_color=(255, 255, 0))
 
     # FPS
     clock = pygame.time.Clock()
@@ -265,7 +259,7 @@ if __name__ == '__main__':
 
     # Scale
     SCALEREADEVENT = pygame.USEREVENT + 1
-    SCALEREADTIME = 500  # milliseconds
+    SCALEREADTIME = 1000  # milliseconds
     pygame.time.set_timer(SCALEREADEVENT, SCALEREADTIME)
 
     # Animation loop
@@ -281,11 +275,11 @@ if __name__ == '__main__':
                     break
             elif event.type == SCALEREADEVENT:
                 weight = scale.check()
-                if weight != 0:
-                    display.is_using_scale = True
-                    scale_frame.set_text(str(weight))
-                elif display.frame_type == 1:
-                    display.is_using_scale = False
+                # if weight != 0:
+                #     display.is_using_scale = True
+                #     scale_read_frame.set_text(str(weight))
+                # elif display.frame_type == 1:
+                #     display.is_using_scale = False
 
         screen.fill((0, 0, 0))
 
