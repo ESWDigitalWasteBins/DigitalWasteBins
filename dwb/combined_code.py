6f6a862dd8560a567186e1c771f722bdcf314c95
khoitd1997 <khoidinhtrinh@gmail.com>
2017-11-22 18:18:41 -0800

    Change read inverval

--- dwb/combined_code.py
+++ dwb/combined_code.py
@@ -259,7 +259,7 @@ if __name__ == '__main__':
 
     # Scale
     SCALEREADEVENT = pygame.USEREVENT + 1
-    SCALEREADTIME = 1000  # milliseconds
+    SCALEREADTIME = 500  # milliseconds
     pygame.time.set_timer(SCALEREADEVENT, SCALEREADTIME)
 
     # Animation loop
