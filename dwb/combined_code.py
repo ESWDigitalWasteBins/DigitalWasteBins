701595ceaf7acf0dc75d8fca88ff051641e4f6ca
Brian Tom <tombl@uci.edu>
2017-12-01 15:06:56 -0800

    Merge

--- dwb/combined_code.py
+++ dwb/combined_code.py
@@ -219,17 +219,19 @@ if __name__ == '__main__':
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
-                # break
-            # elif event.type == SCALEREADEVENT:
-            #     pass
+                break
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+                    break
+            elif event.type == SCALEREADEVENT:
+                weight = my_scale.check()
+                _sw_log(log, "scale", sw)
                 # if weight != 0:
                 #    display.is_using_scale = True
                 #    scale_read_frame.set_text(str(weight))
                 # elif display.frame_type == 1:
                 #    display.is_using_scale = False
-
-        weight = my_scale.check()
-        _sw_log(log, "scale", sw)
 
         screen.fill((0, 0, 0))
         _sw_log(log, "fill", sw)
