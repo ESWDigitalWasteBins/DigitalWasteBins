06395dced291f51d2910d2f365ad6ed250f2da01
khoitd1997 <khoidinhtrinh@gmail.com>
2018-01-01 21:43:27 -0800

    Finalized unit as lbs and delete small increment check

--- dwb/scale.py
+++ dwb/scale.py
@@ -60,7 +60,8 @@ class Scale:
             # Handle second byte
             decimal_point = raw[1] & 0b111
             #current_mode = (raw[1] & 0b11000) >> 3
-            negative = (raw[1] & 0b100000) >> 5
+            negative = (raw[1] & 0b100000)
+            #>> 5
             #self.stable = (raw[1] & 0b1000000) >> 6
             #overflow = (raw[1] & 0b10000000) >> 7
             # TODO: Handle third byte
@@ -76,13 +77,13 @@ class Scale:
             result = float(digit1) + digit2 * 10 + digit3 * 100 + \
                 digit4 * 1000 + digit5 * 10000 + digit6 * 100000
             result /= float(10 ** (decimal_point - 1))  # more precision
+            # Convert from lbs to ounce
+            result = result * 16
             # Handle sixth byte
-            unit = raw[5] & 0b1  # 1 for lbs and 0 for kg
+            # unit = raw[5] & 0b1  # 1 for lbs and 0 for kg
             sw.stop()
             print("TIME (decode): ", sw.read())
-            # convert to ounces
-            # TODO: Finalize the unit to get rid of the if statement
-            result = result * 16 if unit else result * 35.274
+            #result = result * 16 if unit else result * 35.274
             result = result * (-1.0) if negative else result
             # if negative:
             #    result *= (-1.0)
@@ -98,24 +99,24 @@ class Scale:
         # run the scale in STB mode
         # if self.stable == 1:
         # min 0.005 increments, unit is lbs
-        if (self.last_value + 0.0005) < result:
-                    # may need to add check so that people don't pick up thrown in trash and then called it recycled stuffs again
-                    #print("The weight increased")
-            difference = float(result) - float(self.last_value)
-            self.last_value = result
-            sw.stop()
-            self.raw = raw
-            print("TIME (check stable): ", sw.read())
-            # make it easier to see changed weight
-            # print(difference)
-            # time.sleep(3) #turn on for debugging
-            sw.stop()
-            print("TIME (check): ", sw.read())
-            return difference  # return weight change between this and the last stable read in kg
-            #print("the weight stays the same or decreased")
+        # if (self.last_value + 0.0005) < result:
+            # may need to add check so that people don't pick up thrown in trash and then called it recycled stuffs again
+            #print("The weight increased")
+        difference = float(result) - float(self.last_value)
+        self.last_value = result
+        sw.stop()
+        self.raw = raw
+        print("TIME (check stable): ", sw.read())
+        # make it easier to see changed weight
+        # print(difference)
+        # time.sleep(3) #turn on for debugging
+        sw.stop()
+        print("TIME (check): ", sw.read())
+        return difference  # return weight change between this and the last stable read in kg
+        #print("the weight stays the same or decreased")
 
         #print("There is no data")
-        return 0  # weight haven't changed
+        # return 0  # weight haven't changed
 
     # def check(self):
 
