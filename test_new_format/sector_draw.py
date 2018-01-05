14d0acb3a1614fbfe3063542532cbe1fe7e5811a
khoitd1997 <khoidinhtrinh@gmail.com>
2018-01-04 16:02:38 -0800

    adjusted writing header

--- test_new_format/sector_draw.py
+++ test_new_format/sector_draw.py
@@ -19,7 +19,7 @@ class text_surface:
         self._surface = surface_image
         self._color = color
         # self._line_number = line_number
-
+        self._top_offset = top_offset
         self.line_list_rect_stored = []
         for counter in range(0, line_number):
             (self.line_list_rect_stored).append(pygame.Rect(
@@ -30,6 +30,8 @@ class text_surface:
         self._screen.fill(white)
         if header_rect == None:
             self._screen.blit(self._surface, (0, 0))
+        else:
+            self._screen.fill((self._bg_color), (0, self._top_offset))
         counter = 0
         tx_font = self._font
         tx_color = self._color
