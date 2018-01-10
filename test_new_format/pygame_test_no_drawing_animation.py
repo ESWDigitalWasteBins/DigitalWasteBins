f4d654aaaa4dd712422d973b371f64a34af0b60c
khoitd1997 <khoidinhtrinh@gmail.com>
2018-01-10 15:51:07 -0800

    added more pump and new font

--- test_new_format/pygame_test_no_drawing_animation.py
+++ test_new_format/pygame_test_no_drawing_animation.py
@@ -66,7 +66,7 @@ if __name__ == '__main__':
 
     # load images given by Tyson
     # TODO: Refactor the loading sections
-
+    pygame.event.pump()
     #----------------------------------------------------
     # used for selecting which mode to be in
     m = 'r'  # l for landfill, r for recycle and c for compost
@@ -121,7 +121,7 @@ if __name__ == '__main__':
         background_color = blue
         # surface_left_offset = 20
         # surface_top_offset = 20
-
+    pygame.event.pump()
     for i in range(0, total_image):
         im.append(pygame.image.load(os.path.join(
             'test_new_format', m + str(i) + '.png')))
@@ -129,7 +129,7 @@ if __name__ == '__main__':
 
     # Divide each section of the screen into many small squares to
     # draw gradually instead at once
-
+    pygame.event.pump()
     toprect_offset_im = []
     midrect_offset_im = []
     botrect_offset_im = []
@@ -144,6 +144,7 @@ if __name__ == '__main__':
         toprect_offset_im.append(toprect)
         midrect_offset_im.append(midrect)
         botrect_offset_im.append(botrect)
+        pygame.event.pump()
 
     # rectange used for deleting before redraw of sections
     section_num = 3
