d0e75d0b40744412b7710f64b6904409e3986458
khoitd1997 <khoidinhtrinh@gmail.com>
2018-01-10 18:01:28 -0800

    combined threading and no animation code

--- test_new_format/pygame_test_threading.py
+++ test_new_format/pygame_test_threading.py
@@ -14,7 +14,6 @@ if __name__ == '__main__':
     #----------------------------------------------------
     # intialize important things here
     pygame.init()
-
     # full screen
     screen = pygame.display.set_mode(
         (0, 0), pygame.FULLSCREEN)
@@ -25,22 +24,20 @@ if __name__ == '__main__':
     size_per_line = font.get_linesize()
     start = time.time()  # start of timer for when to draw
 
-    scale_lock = threading.RLock()
-
     #----------------------------------------------------
     # dictate the width, length and number of squares
     # all units are in pixel for this section
-    square_length = 30  # the length of each small square in the sector
+    square_length = 20  # the length of each small square in the sector
 
     # number of squares in each sector=list_length_vertical *list_length_horizontal
-    list_length_vertical = 13
-    list_length_horizontal = 60
+    list_length_vertical = 14
+    list_length_horizontal = 85
 
     # total length of each sector square, used for allocating blank surface to draw on, usually allocate with a little headroom
     total_square_vertical_length = square_length * list_length_vertical
     total_square_horizontal_length = square_length * list_length_horizontal
     x_offset = 0  # x offset of the sector of the screen
-    y_offset_top = size_per_line  # y offset of the sector of the screen
+    y_offset_top = 2 * size_per_line  # y offset of the sector of the screen
     y_offset_bot = 200
     top_header_width = screen.get_width()
     top_header_height = 200
@@ -70,10 +67,10 @@ if __name__ == '__main__':
 
     # load images given by Tyson
     # TODO: Refactor the loading sections
-
+    pygame.event.pump()
     #----------------------------------------------------
     # used for selecting which mode to be in
-    m = 'c'  # l for landfill, r for recycle and c for compost
+    m = 'r'  # l for landfill, r for recycle and c for compost
     # set mode of running
     surface_left_offset = 70
     surface_top_offset = 0
@@ -111,7 +108,7 @@ if __name__ == '__main__':
         # surface_top_offset = 20
         total_image = 9
     elif m == 'r':
-        header_offset = -350
+        header_offset = -530
         text_box_im = pygame.image.load((os.path.join(
             'test_new_format', 'bt' + '.png')))
         text_box_im.convert()
@@ -125,7 +122,7 @@ if __name__ == '__main__':
         background_color = blue
         # surface_left_offset = 20
         # surface_top_offset = 20
-
+    pygame.event.pump()
     for i in range(0, total_image):
         im.append(pygame.image.load(os.path.join(
             'test_new_format', m + str(i) + '.png')))
@@ -133,23 +130,31 @@ if __name__ == '__main__':
 
     # Divide each section of the screen into many small squares to
     # draw gradually instead at once
-
+    pygame.event.pump()
+    screen.fill(white)
+    pygame.display.flip()
     toprect_offset_im = []
     midrect_offset_im = []
-    # bot_rect_offset_im = []
+    botrect_offset_im = []
     for k in im:
         toprect = Rect((screen.get_width() - k.get_width()) //
-                       2, y_offset_top + 30, total_square_horizontal_length, total_square_vertical_length)
+                       2, y_offset_top, total_square_horizontal_length, total_square_vertical_length)
         midrect = Rect((screen.get_width() - k.get_width()) //
                        2, y_offset_top + total_square_vertical_length, total_square_horizontal_length, total_square_vertical_length)
+        botrect = Rect((screen.get_width() - k.get_width()) //
+                       2, y_offset_top + 2 * total_square_vertical_length, total_square_horizontal_length, total_square_vertical_length)
         toprect_offset_im.append(toprect)
         midrect_offset_im.append(midrect)
+        botrect_offset_im.append(botrect)
+        pygame.event.pump()
 
     # rectange used for deleting before redraw of sections
     section_num = 3
     top_rect = Rect(0, y_offset_top, screen.get_width(),
                     total_square_vertical_length)
-    mid_rect = Rect(0, y_offset_bot + total_square_vertical_length, screen.get_width(),
+    mid_rect = Rect(0, y_offset_top + total_square_vertical_length, screen.get_width(),
+                    total_square_vertical_length)
+    bot_rect = Rect(0, y_offset_top + 2 * total_square_vertical_length, screen.get_width(),
                     total_square_vertical_length)
 
     # textbox image
@@ -159,30 +164,19 @@ if __name__ == '__main__':
     # Initializing Top and Bottom header
 
     top_header = text_surface(
-        screen, screen, 1, screen.get_width() / 2 + header_offset, 0, 0, 0, white, "", background_color, True)
-
-    # bot_header = text_surface(
-    #     screen, screen, 1, header_offset + screen.get_width() / 2 + compensation, 400, white, "", black, True)
+        screen, screen, 1, screen.get_width() / 2 + header_offset, 0.25 * dist_btw_line, 0, 0, white, "", background_color, True)
 
     # begin with a white color
     screen.fill(white)
     pygame.event.pump()  # used for keeping the OS happy
-    pygame.display.flip()
-    # testing = True
-    # TODO: Refactor the code below
-    # the code below will cycle through screen sector as well
-    # as loaded images and display them with a defined time interval
-    # the images are displayed gradually in order to create transition effects
-    # as well as alleviate the load on the pi CPU
 
-    # display initial image first
-    # bot_header.draw_text_surface(bot_header_text)
+    # draw header first
+    scale_lock = threading.RLock()
     scale_thread = Scale_Thread(
         screen, scale_lock, text_box_class, top_header, top_header_text)
     top_header.draw_text_surface(top_header_text)
     pygame.display.flip()
     scale_thread.start()
-
     # weight = 5  # only for testing
     while (not(exited)):
         # pygame.event.pump()
@@ -191,23 +185,29 @@ if __name__ == '__main__':
                 if (event.key == pygame.K_ESCAPE):
                     exited = True
 
+        # if l%3==0:
+        #     text_box_class.draw_text_surface(compost_text_processing(5))
+        #     pygame.display.flip()
+
         if (time.time() - start) > screen_update_interval:
-            # wait if the other thread is using the screen
-            scale_lock.acquire()
             start = time.time()
-            if current_pos == section_num - 1:
-                current_pos = 0
+            if current_pos == 0:
+                current_pos = +1
                 pygame.event.pump()
                 screen.fill((white), top_rect)
                 screen.blit(im[l], toprect_offset_im[l])
                 pygame.display.update(top_rect)
-
-            else:
+            elif current_pos == 1:
                 current_pos += 1
                 pygame.event.pump()
                 screen.fill((white), mid_rect)
                 screen.blit(im[l], midrect_offset_im[l])
                 pygame.display.update(mid_rect)
-            scale_lock.release()
+            elif current_pos == 2:
+                current_pos = 0
+                pygame.event.pump()
+                screen.fill((white), bot_rect)
+                screen.blit(im[l], botrect_offset_im[l])
+                pygame.display.update(bot_rect)
             l = l + 1 if l < total_image - 1 else 0
     pygame.quit()
