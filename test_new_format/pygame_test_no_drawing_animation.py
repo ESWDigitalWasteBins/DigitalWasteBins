7bcca4b211106b0c417224ce911b258e107094ae
khoitd1997 <khoidinhtrinh@gmail.com>
2018-01-10 16:47:05 -0800

    increased vertical length

--- test_new_format/pygame_test_no_drawing_animation.py
+++ test_new_format/pygame_test_no_drawing_animation.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     square_length = 20  # the length of each small square in the sector
 
     # number of squares in each sector=list_length_vertical *list_length_horizontal
-    list_length_vertical = 13
+    list_length_vertical = 14
     list_length_horizontal = 85
 
     # total length of each sector square, used for allocating blank surface to draw on, usually allocate with a little headroom
